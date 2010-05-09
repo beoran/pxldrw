1c052d35da399dafb98172752d870ed781ad5fc5
Beoran <beoran@rubyforge.org>
2010-05-09 23:10:31 +0200

    Trying SWT to see if it helps the speed issue.

--- src/pxldrw/Main.java
+++ src/pxldrw/Main.java
@@ -10,6 +10,10 @@ import javax.swing.UIManager;
 import pxldrw.gui.*;
 import pxldrw.logic.*;
 import pxldrw.logic.Palette;
+import static pxldrw.tools.Puts.*;
+import org.eclipse.swt.layout.RowLayout;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
 
 
 
@@ -23,27 +27,41 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        Palette pal         = Palette.make();
+        Grid grid           = new Grid(640, 640, pal);
+
+    	Display display 	= new Display();
+    	Shell shell 		= new Shell(display);
+    	//shell.setLayout( new RowLayout());
+    	shell.setText("Hi SWT!");     	
+     	//shell.pack();
+    	shell.open();
+    	while(!shell.isDisposed()) {
+    	      if(!display.readAndDispatch()) 
+    	      display.sleep();
+    	}
+    	display.dispose();
+/*
         try {
         UIManager.setLookAndFeel(
             UIManager.getSystemLookAndFeelClassName());
         } catch (Exception ex) {
-            puts("Could not set look and feel.")
+            puts("Could not set look and feel.");
         }        
         Frame frame         = new Frame("pxldrwj", 640, 480);
         Subframe sframe     = new Subframe("Edit", 10, 10);
         Desk desk           = new Desk();
-        Palette pal         = Palette.make();
-        Grid grid           = new Grid(640, 640, pal);
         Gridedit gridedit   = new Gridedit(grid, 3);
         JScrollPane scroll  = new JScrollPane(gridedit);
-
+*/        
         /* puts("Hello!" , "World!"); */
         /*
         ascrlf.Tryout a     = new ascrlf.Tryout();
         a.hello();
          *
          */
-        pal.add();
+/*
+    	pal.add();
         //pal.add(255,255,255);
         pal.add(0,0,0);
         grid.fill(0);
@@ -56,12 +74,12 @@ public class Main {
         frame.addChild(sframe);
         // frame.setSize(gridedit.wide, gridedit.high);
         // root.setSize(640, 480);
-        
+  */      
         
 
-
+    	/*
         frame.setVisible(true);
-        /*root.add(frame);
+        root.add(frame);
         */
         
     }
